98801c3d7a6a158c4386e04a54a91fe712a91bec
harisson.nas@gmail.com <harisson.nas@gmail.com>
2020-10-02 16:01:49 -0300

    Creation of the APIUtil enum

---
+++ jsf-frontend/src/main/java/br/com/harisson/jsffrontend/util/APIUtil.java
@@ -0,0 +1,40 @@
+package br.com.harisson.jsffrontend.util;
+
+import lombok.Getter;
+
+@Getter
+public enum APIUtil {
+    URL_VEHICLE_BASE("http://localhost:8082/gateway/spring-backend/v1/vehicle"),
+    URL_VEHICLE_POST(concatUrlVehicleBaseWithComplement("/admin")),
+    URL_VEHICLE_PUT(concatUrlVehicleBaseWithComplement("/admin")),
+    URL_VEHICLE_DELETE_BY_ID(concatUrlVehicleBaseWithComplement("/admin/{id}")),
+    URL_VEHICLE_LIST_ALL(concatUrlVehicleBaseWithComplement("/listAllVehicles")),
+    URL_VEHICLE_LIST_IN_STOCK(concatUrlVehicleBaseWithComplement("/listVehiclesInStock")),
+    URL_VEHICLE_LIST_SOLD(concatUrlVehicleBaseWithComplement("/listSoldVehicles")),
+    URL_VEHICLE_LIST_BY_MODEL(concatUrlVehicleBaseWithComplement("/listVehiclesByModel")),
+    URL_VEHICLE_FIND_BY_ID(concatUrlVehicleBaseWithComplement("/findVehicleById/{id}")),
+    // ----------------------------------------------------------------------------------
+    URL_BUYER_BASE("http://localhost:8082/gateway/spring-backend/v1/buyer"),
+    URL_BUYER_POST(concatUrlBuyerBaseWithComplement("/admin")),
+    URL_BUYER_PUT(concatUrlBuyerBaseWithComplement("/admin")),
+    URL_BUYER_DELETE_BY_ID(concatUrlBuyerBaseWithComplement("/admin/{id}")),
+    URL_BUYER_LIST_ALL(concatUrlBuyerBaseWithComplement("/listAllBuyers")),
+    URL_BUYER_LIST_NON_CONTACTED(concatUrlBuyerBaseWithComplement("/listNonContactedBuyers")),
+    URL_BUYER_LIST_CONTACTED(concatUrlBuyerBaseWithComplement("/listContactedBuyers")),
+    URL_BUYER_FIND_BY_VEHICLE(concatUrlBuyerBaseWithComplement("/findBuyersByVehicle")),
+    URL_BUYER_FIND_BY_ID(concatUrlBuyerBaseWithComplement("/findById/{id}"));
+
+    private final String urlBase;
+
+    APIUtil(String urlBase) {
+        this.urlBase = urlBase;
+    }
+
+    private static String concatUrlVehicleBaseWithComplement(String complement) {
+        return APIUtil.URL_VEHICLE_BASE.getUrlBase().concat(complement);
+    }
+
+    private static String concatUrlBuyerBaseWithComplement(String complement) {
+        return APIUtil.URL_BUYER_BASE.getUrlBase().concat(complement);
+    }
+}
